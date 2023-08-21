3814d71e18a770f3e858ff464da7980629eacdf1
Saliha.M2i <saliha.belayachi@gmail.com>
2023-08-21 10:41:56 +0200

    21 aout

--- jdbc/src/main/java/org/example/compteBancaire/Main.java
+++ jdbc/src/main/java/org/example/compteBancaire/Main.java
@@ -3,8 +3,8 @@ package org.example.compteBancaire;
 public class Main {
 
     public static void main(String[] args) {
-        
-    }
+        System.out.println("Hello World");
 //        ConsoleIhm.start();
+    }
 
 }
